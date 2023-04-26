191d4f9fe1a997d74f3aad36fc58b1beb92f94d8
anthonyLuisSotoColling <govips@gmail.com>
2023-04-25 17:52:55 -0700

    did lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(cards, message) {
+    const giftMessages = []
+    for (let i = 0; i <= writeCards.length; i++) {
+        giftMessages.push(`Thank you, ${cards[i]}, for the wonderful ${message} gift!`);
+    }
+    return giftMessages;
+}
+writeCards(cards, "suprise");
+
+let cookies = 10
+function countDown(cookies) {
+    let i = 0;
+    while (cookies >= 0) {
+        console.log(cookies--);
+    }
+}
